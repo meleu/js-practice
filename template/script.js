8e020501da3c7d65c66ded046d54fe6227048b32
meleu <meleuzord@yahoo.com>
2020-09-18 19:48:45 -0300

    smallfix

--- template/script.js
+++ template/script.js
@@ -1,5 +1,5 @@
 function githubLink() {
   const projectName = 'template';
-  const githubUrl = 'https://github.com/meleu/js-practice/tree/master/';
+  const githubUrl = 'https://github.com/meleu/js-practice/tree/master';
   window.open(`${githubUrl}/${projectName}`, '_blank');
 }
